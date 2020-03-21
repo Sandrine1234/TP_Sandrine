b53fa0994e19f62e2efe192d9db35a574e9ef493
Sandrine1234 <62106780+Sandrine1234@users.noreply.github.com>
2020-03-21 15:15:11 -0400

    Définition line namer

--- quoridor.py
+++ quoridor.py
@@ -10,3 +10,14 @@ def analyser_commande():
     # parser.add_argument('bar', type = str, help = 'Argument positionnel de la sous-commande acheter')
     return parser.parse_args()
 
+# pour la deuxième fonction
+def line_namer(i):
+    b = []
+    if (((i + 1) // 2) + 1) < 10:
+        b = ["{} |".format(((i + 1) // 2) + 1)]
+    # si respecte pas la première condition
+    else:
+        b = ["{}|".format(((i + 1) // 2) + 1)]
+    # retourne une liste
+    return b
+
