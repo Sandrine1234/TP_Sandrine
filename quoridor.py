67a1c4f4cb983809f1e22f9cf0ab1e30e9652ea4
Sandrine1234 <62106780+Sandrine1234@users.noreply.github.com>
2020-03-21 15:14:33 -0400

    Définition analyser commande

--- quoridor.py
+++ quoridor.py
@@ -1,1 +1,12 @@
+import argparse
+
+
+# définition de la première fonction analyser commande
+def analyser_commande():
+    # module parser est un module qui fournit une interface à l'analyseur interne de Python
+    parser = argparse.ArgumentParser(description = "Jeu quoridor - Phase 1")
+    parser.add_argument('-l', '--lister', action = 'store_true', help = 'Lister les identifiants de vos 20 dernières parties.')
+    parser.add_argument('idul', metavar='idul', type = str, help = 'IDUL du joueur')
+    # parser.add_argument('bar', type = str, help = 'Argument positionnel de la sous-commande acheter')
+    return parser.parse_args()
 
